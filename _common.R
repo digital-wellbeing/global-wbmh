9d4e6eb76ac21b60f9d22f5c6c8b11b2f8e397b9
Matti Vuorre <vuorre@gmail.com>
2022-01-10 13:55:47 +0900

    Split into sections

---
+++ _common.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+# Plotting theme & use this font if available
+Font <- "Titillium Web"
+theme_set(
+  theme_linedraw(
+    base_size = 12,
+    base_family = Font
+  ) +
+    theme(
+      panel.grid = element_blank(),
+      strip.text = element_text(margin = margin(4, 4, 4, 4, "pt"))
+    )
+)
+
+library(knitr)
+opts_chunk$set(
+  cache = TRUE,
+  cache.lazy = FALSE
+)
