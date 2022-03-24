79fa6214c5d85f1e932b65b6ed7cc13e995d3a0a
Matti Vuorre <vuorre@gmail.com>
2022-03-24 16:19:03 +0000

    Refactor

--- _common.R
+++ _common.R
@@ -1,16 +1,23 @@
 library(ggplot2)
-# Plotting theme & use this font if available
-Font <- "Titillium Web"
+library(ragg)
+library(khroma)
+
+# Plotting options
+# If you've defined a custom font, we will use it in outputs
+Font <- Sys.getenv("FONT")
 theme_set(
   theme_linedraw(
-    base_size = 12,
+    base_size = 9,
     base_family = Font
   ) +
     theme(
       panel.grid = element_blank(),
-      strip.text = element_text(margin = margin(4, 4, 4, 4, "pt"))
+      strip.text = element_text(margin = margin(4, 4, 4, 4, "pt")),
+      axis.text = element_text(size = rel(0.75))
     )
 )
+# Width of figures
+W <- 9
 
 library(knitr)
 opts_chunk$set(
