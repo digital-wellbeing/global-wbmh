9e813e6e73cfca3f4fef9bfea8590954697354bf
Matti Vuorre <vuorre@gmail.com>
2022-05-06 15:48:46 +0100

    Get notified when job ends

--- submit.sh
+++ submit.sh
@@ -8,6 +8,8 @@
 #SBATCH --mem-per-cpu=3G
 #SBATCH --time=7-00:00:00
 #SBATCH --job-name=DWG-Global
+#SBATCH --mail-type=END
+#SBATCH --mail-user=matti.vuorre@oii.ox.ac.uk
 
 module purge
 module load R/4.1.2-foss-2021b-ARC
