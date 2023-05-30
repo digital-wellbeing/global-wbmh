8856958e07faf448d090d232b00172b7054cf02c
Matti Vuorre <vuorre@gmail.com>
2023-05-30 13:07:21 +0200

    Include cell Ns as weights

--- models.R
+++ models.R
@@ -13,7 +13,7 @@ library(tidyr)
 library(brms)
 
 # MCMC settings
-options(brms.backend = "rstan")
+options(brms.backend = "cmdstanr")
 hmc <- list(
   save_pars = save_pars(group = c("age", "country")),
   chains = 4,
@@ -53,7 +53,7 @@ fits <- dat %>%
 
 # Time only
 bf_t <- bf(
-  val | se(se, sigma = TRUE) + subset(!is.na(val)) ~
+  val | se(sd, sigma = TRUE) + weights(n) + subset(!is.na(val)) ~
     year * sex +
     (year * sex | country) +
     (year * sex | age) +
@@ -61,29 +61,49 @@ bf_t <- bf(
   family = gaussian()
 )
 # Simplified model for mental health outcomes
-bf_t.s <- update(bf_t, ~ . -(year * sex | age:country))
+bf_t.s <- bf(
+  val | se(sd, sigma = TRUE) + subset(!is.na(val)) ~
+    year * sex +
+    (year * sex | country) +
+    (year * sex | age),
+  family = gaussian()
+)
 
-# Univariate outcome on time and internet
+# Time and internet
 bf_i <- bf(
-  val | se(se, sigma = TRUE) ~
+  val | se(sd, sigma = TRUE) + weights(n) ~
     (year + i_cw) * sex + i_cb +
     ((year + i_cw) * sex | country) +
     ((year + i_cw) * sex | age) +
     ((year + i_cw) * sex | age:country),
   family = gaussian()
 )
-bf_i.s <- update(bf_i, ~ . -((year + i_cw) * sex | age:country))
+# Simplified model for mental health outcomes
+bf_i.s <- bf(
+  val | se(sd, sigma = TRUE) ~
+    (year + i_cw) * sex + i_cb +
+    ((year + i_cw) * sex | country) +
+    ((year + i_cw) * sex | age),
+  family = gaussian()
+)
 
-# Univariate outcome on time and mobile
+# Time and mobile
 bf_m <- bf(
-  val | se(se, sigma = TRUE) ~
+  val | se(sd, sigma = TRUE) + weights(n) ~
     (year + m_cw) * sex + m_cb +
     ((year + m_cw) * sex | country) +
     ((year + m_cw) * sex | age) +
     ((year + m_cw) * sex | age:country),
   family = gaussian()
 )
-bf_m.s <- update(bf_m, ~ . -((year + m_cw) * sex | age:country))
+# Simplified model for mental health outcomes
+bf_m.s <- bf(
+  val | se(sd, sigma = TRUE) ~
+    (year + m_cw) * sex + m_cb +
+    ((year + m_cw) * sex | country) +
+    ((year + m_cw) * sex | age),
+  family = gaussian()
+)
 
 # Pick the row to fit
 fits <- fits %>%
@@ -102,18 +122,6 @@ fits <- fits %>%
     )
   ) %>%
   arrange(outcome, model)
-
-# Priors
-p1 <- prior(normal(0, 1), class = "b") +
-  prior(normal(0, 3), class = "b", coef = "sex1")
-p2 <- prior(normal(0, 1), class = "b") +
-  prior(normal(0, 3), class = "b", coef = "sex1") +
-  prior(normal(0, 0.5), class = "sd", coef = "i_cw", group = "age") +
-  prior(lkj(1), class = "cor", group = "age")
-p3 <- prior(normal(0, 1), class = "b") +
-  prior(normal(0, 3), class = "b", coef = "sex1") +
-  prior(normal(0, 0.5), class = "sd", coef = "m_cw", group = "age") +
-  prior(lkj(1), class = "cor", group = "age")
 
 
 # Center predictors -------------------------------------------------------
@@ -137,12 +145,10 @@ fits$data[[1]] <- fits$data[[1]] %>%
     across(c(i_cb, m_cb), ~. - mean(., na.rm = TRUE)),
   )
 
-
 # fit model ---------------------------------------------------------------
 
 hmc$data <- fits$data[[1]]
 hmc$formula <- fits$bfrm[[1]]
-hmc$prior <- list(p1, p2, p3)[[fits$model]]
 hmc$file <- paste0(path, "/brm-", fits$outcome[[1]], "-", fits$model[[1]])
 
 cat("\n\nNow fitting", hmc$file, "\n\n")
